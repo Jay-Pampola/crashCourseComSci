0085dec26a6953be4febbef849dee7e05e23d47a
Mielbert Jay Pampola <mil.pampola112311@gmail.com>
2022-10-07 17:01:18 +0800

    REACT.js

--- REACT02.js
+++ REACT02.js
@@ -16,3 +16,24 @@ class Lottery extends React.Component {
     }
 }
 */
+
+class Machine extends React.Component {
+    render() {
+        const { s1, s2, s3 } = this.props;
+        const winner = (s1 === s2) && (s2 === s3);
+
+        return (
+            <div className="Machine">
+                <b>{s1}</b> <b>{s2}</b> <b>{s3}</b>
+                <p>You {winner ? "win!" : "lose!"}</p>
+            </div>
+        );
+    }
+}
+
+/*
+ReactDOM.render(
+  <Machine s1="🍇" s2="🍇" s3="🍇" />,
+  document.getElementById("root")
+);S
+*/
